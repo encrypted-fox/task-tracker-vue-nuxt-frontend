5d89a1eca8ad8d8fc1d868eacad1b0e1f38820c1
encrypted_fox <yaroslav.souchkov@gmail.com>
2024-08-27 20:57:20 +0300

    Add configuration for prettier and eslint and execute lintfix

--- tests/e2e/main.test.ts
+++ tests/e2e/main.test.ts
@@ -1,13 +1,13 @@
-import { setup, createPage } from '@nuxt/test-utils/e2e'
-import { describe, it, expect } from 'vitest'
+import { setup, createPage } from "@nuxt/test-utils/e2e";
+import { describe, it, expect } from "vitest";
 
-describe('app renders', async () => {
+describe("app renders", async () => {
   await setup({
-    host: 'http://localhost:3000',
-  })
+    host: "http://localhost:3000",
+  });
 
-  it('displays heading', async () => {
-    const page = await createPage('/')
-    expect(await page.getByRole('heading').isVisible()).toBe(true)
-  })
-})
+  it("displays heading", async () => {
+    const page = await createPage("/");
+    expect(await page.getByRole("heading").isVisible()).toBe(true);
+  });
+});
