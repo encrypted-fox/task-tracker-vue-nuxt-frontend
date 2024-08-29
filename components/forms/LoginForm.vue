a6dde0862f5bf8e078584fef608bf388bad641d5
encrypted_fox <yaroslav.souchkov@gmail.com>
2024-08-30 00:55:53 +0300

    Move and adaptate Login and Register form components

---
+++ components/forms/LoginForm.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+.auth__form.flex.border-styling.w-350px.flex-col.gap-25px
+  .auth__form-header.py-25px.division-styling.border-0.border-b-1px
+    h1.auth__form__title.text-center.text-styling.m-0 Вход
+  form.auth__form-content.flex.flex-col.gap-15px.px-25px
+    .field
+      label.field-label(for="login") Логин
+      input.field-string.w-full(id="login" placeholder="Начните писать..." :value="login" @input="changeLogin")
+    .field
+      label.field-label(for="password") Пароль
+      input.field-string.w-full(id="password" type="password" placeholder="Начните писать..." :value="password" @input="changePassword") 
+        
+  .auth__form-footer.flex.justify-between.px-6.pb-6
+    button.btn-lg.btn-secondary(@click="switchToRegister") Зарегистрироваться
+    button.btn-lg.btn-primary(@click="submit" :disabled="isButtonDisabled") Войти
+</template>
+<script setup lang="ts">
+const emit = defineEmits(['switch-to-register'])
+
+const login = useState<string>("login-form__login");
+const password = useState<string>("login-form__password");
+
+const isButtonDisabled = computed(() => {
+  return !password.value || !login.value
+})
+
+const changeLogin = (e: Event) => {
+  const val = (e.target as HTMLInputElement).value;
+
+  login.value = val
+};
+
+const changePassword = (e: Event) => {
+  const val = (e.target as HTMLInputElement).value;
+
+  password.value = val;
+};
+
+const switchToRegister = () => {
+  emit('switch-to-register')
+}
+
+useHead(<any>{
+  title: 'Вход'
+})
+</script>
--- components/forms/RegisterForm.vue
+++ components/forms/RegisterForm.vue
@@ -1,46 +1,46 @@
 <template lang="pug">
-.auth__form.w-350px.flex.flex-column.gap-25.radius-6
-  .auth__form-header.pv-25
-    h1.auth__form__title.text-center Регистрация
-  form.auth__form-content.flex.flex-column.gap-20.ph-25
+.auth__form.flex.border-styling.rounded-md.w-350px.flex.flex-col.gap-25px
+  .auth__form-header.py-25px.division-styling.border-0.border-b-1px
+    h1.auth__form__title.text-center.text-styling.m-0 Регистрация
+  form.auth__form-content.flex.flex-col.gap-15px.px-25px
     .field
-      label.field-label.block(for="login") Логин
-      input.field-input(id="login" :value="login" @input="changeLogin" :class="{'field-input-input--error': isLoginTooShort}")
-      Transition(name='fade' mode='out-in')
-        .field-input-error(v-if="isLoginTooShort")
+      label.field-label(for="login") Логин
+      input.field-string.w-full(id="login" placeholder="Начните писать..." :value="login" @input="changeLogin" :class="{'field-invalid': isLoginTooShort}")
+      Transition(name='disappear-element' mode='out-in')
+        .field-error(v-if="isLoginTooShort")
           | {{ loginError }}
     .field
-      label.field-label.block(for="password") Пароль
-      input.field-input(id="password" type="password" :value="password" @input="changePassword" :class="{'field-input-input--error': isWeak}") 
-    .field
-      label.field-label.block(for="repeatPassword") Повторить пароль
-      input.field-input(id="repeatPassword" type="password" :value="repeatPassword" @input="changeRepeatPassword" :class="{'field-input-input--error': isPasswordNotMatch}") 
+      label.field-label(for="password") Пароль
+      input.field-string.w-full(id="password" type="password" placeholder="Начните писать..." :value="password" @input="changePassword" :class="{'field-invalid': isWeak}") 
       Transition(name='fade' mode='out-in')
         .field-input-error(v-if="isWeak || isPasswordNotMatch")
-          TransitionGroup(name='fade')
-            .field-input-error(v-for="(val) in currentErrors", :key="val.name")
+          TransitionGroup(name='disappear-element')
+            .field-error(v-for="(val) in currentErrors", :key="val.name")
               | {{ val.label }}
+    .field
+      label.field-label(for="repeatPassword") Повторить пароль
+      input.field-string.w-full(id="repeatPassword" type="password" placeholder="Начните писать..." :value="repeatPassword" @input="changeRepeatPassword" :class="{'field-input-input--error': isPasswordNotMatch}") 
         
-  .auth__form-footer.flex.justify-between.ph-25.pb-25
-    button.btn.btn-secondary(@click="switchToLogin") Войти
-    button.btn.btn-primary(@click="submit" :disabled="isButtonDisabled") Зарегистрироваться
+  .auth__form-footer.flex.justify-between.px-25px.pb-25px
+    button.btn-lg.btn-secondary(@click="switchToLogin") Войти
+    button.btn-lg.btn-primary(@click="submit" :disabled="isButtonDisabled") Зарегистрироваться
 </template>
 
 <script setup lang="ts">
-import type { ErrorMessages  } from '~/types';
+import type { AuthErrorMessages  } from '~/types';
 import { passwordErrors, loginError } from '~/utils/errors';
 
 const emit = defineEmits(['switch-to-login'])
 
-const login = useState<string>("login");
-const password = useState<string>("password");
-const repeatPassword = useState<string>("repeatPassword");
+const login = useState<string>("register-form__login");
+const password = useState<string>("register-form__password");
+const repeatPassword = useState<string>("register-form__repeatPassword");
 
 const isWeak = useState<boolean>("isWeak", () => false);
 const isPasswordNotMatch = useState<boolean>("isPasswordNotMatch", () => false);
 const isLoginTooShort = useState<boolean>("isLoginTooShort", () => false);
 
-const errorMessages = useState<ErrorMessages>("errorMessages", () => ({
+const errorMessages = useState<AuthErrorMessages>("errorMessages", () => ({
   no2UpperCase: false,
   no3LowerCase: false,
   no2Digits: false,
@@ -132,7 +132,7 @@ const changeRepeatPassword = (e: Event) => {
 
   if (isPasswordNotMatch.value) console.log("weak");
 
-  password.value = val;
+  repeatPassword.value = val;
 };
 
 const switchToLogin = () => {
@@ -142,22 +142,4 @@ const switchToLogin = () => {
 useHead(<any>{
   title: 'Регистрация'
 })
-
 </script>
-<style lang="scss" scoped>
-@use '~/assets/scss/colors.scss' as *;
-
-.auth__form {
-  border: 1px solid $grey;
-
-  &-header {
-    border-bottom: 1px solid $grey;
-  }
-
-  &__title {
-    font-size: 24px;
-    font-weight: 600;
-    margin: 0;
-  }
-}
-</style>
