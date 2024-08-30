977dc98a2c9f0d3e2213330944f9abaaa35819b0
encrypted_fox <yaroslav.souchkov@gmail.com>
2024-08-30 23:48:48 +0300

    Add some linting and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
-import withNuxt from "./.nuxt/eslint.config.mjs";
+import withNuxt from './.nuxt/eslint.config.mjs'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-export default withNuxt();
+export default withNuxt(
+  {
+    settings: {
+      'vue/setup-compiler-macros': true, // Включает поддержку `<script setup>`
+    },
+
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module',
+    },
+
+    rules: {
+      semi: 'error',
+      'prefer-const': 'error',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
+  eslintPluginPrettierRecommended
+)
 // Your custom configs here
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
-import { defineNuxtConfig } from 'nuxt/config';
+// @ts-nocheck
+import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: '2024-04-03',
   devtools: {
     enabled: true,
 
@@ -11,17 +12,23 @@ export default defineNuxtConfig({
     },
   },
   app: {
-    pageTransition: { name: 'page', mode: 'out-in' }
+    pageTransition: { name: 'page', mode: 'out-in' },
   },
-  modules: ["@pinia/nuxt", "@nuxt/test-utils", "@unocss/nuxt", "@nuxt/eslint"],
-  
+  modules: [
+    '@pinia/nuxt',
+    '@nuxt/test-utils',
+    '@unocss/nuxt',
+    '@nuxt/eslint',
+    '@pinia-plugin-persistedstate/nuxt',
+  ],
+  buildModules: ['@nuxtjs/svg'],
   routeRules: {
     '/auth': { prerender: true },
     '/auth/login': {
-      redirect: { to: '/auth', statusCode: 302 }
+      redirect: { to: '/auth', statusCode: 302 },
     },
     '/auth/register': {
-      redirect: { to: '/auth', statusCode: 302 }
-    }
+      redirect: { to: '/auth', statusCode: 302 },
+    },
   },
 })
