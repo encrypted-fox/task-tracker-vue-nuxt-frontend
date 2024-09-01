53e126d7a9c63bc08f4aaee7a3da756868cb3e93
encrypted_fox <yaroslav.souchkov@gmail.com>
2024-09-01 12:27:05 +0300

    Add notifications styles

--- uno.config.ts
+++ uno.config.ts
@@ -21,8 +21,7 @@ export default <UnocssNuxtOptions>defineConfig({
     'border-styling': 'main-border-styling border-coloring',
 
     // buttons
-    'btn-hover': 'hover:opacity-75 hover:transition-all hover:duration-200',
-    btn: 'main-border-styling focus:border-indigo-500 active:opacity-100 disabled:cursor-not-allowed cursor-pointer transition-all duration-200 btn-hover',
+    btn: 'main-border-styling hover:opacity-85 focus:border-indigo-500 active:opacity-100 disabled:cursor-not-allowed cursor-pointer transition-all duration-300',
     'btn-xs': 'text-xs px-8px py-4px',
     'btn-sm': 'text-xs px-8px py-4px',
     'btn-md': 'text-sm px-10px py-6px',
@@ -32,17 +31,17 @@ export default <UnocssNuxtOptions>defineConfig({
     'btn-round-md': 'p-6px !rounded-full w-20px h-20px',
     'btn-round-lg': 'p-8px !rounded-full w-25px h-25px',
     'btn-round-xl': 'p-10px !rounded-full w-25px h-25px',
-    link: 'w-fit text-indigo-500 text-sm hover:text-indigo-400 active:text-indigo-500 cursor-pointer',
-
-    // dark:disabled:bg-slate-700 dark:disabled:text-slate-50
+    link: 'w-fit transition-all duration-300 text-indigo-500 text-sm hover:text-indigo-400 active:text-indigo-500 cursor-pointer',
     'btn-primary-disabled':
       'disabled:bg-indigo-300 dark:disabled:bg-indigo-950 dark:disabled:text-indigo-500',
     'btn-primary':
-      'bg-indigo-500 text-slate-50 btn-primary-disabled btn focus:!border-slate-50',
+      'bg-indigo-500 text-slate-50 fill-slate-50 btn-primary-disabled btn focus:!border-orange-300 dark:focus:!border-slate-50',
     'btn-secondary-disabled': 'dark:bg-indigo-950 dark:text-indigo-500 btn',
-    'btn-secondary-dark': 'dark:bg-gray-700 dark:text-slate-50 btn',
+    'btn-secondary-dark':
+      'dark:bg-gray-700 dark:fill-slate-50 dark:text-slate-50 btn',
     'btn-secondary':
-      'bg-indigo-50 text-indigo-500 btn-secondary-disabled btn-secondary-dark btn',
+      'bg-indigo-50 fill-indigo-500 text-indigo-500 btn-secondary-disabled btn-secondary-dark btn',
+    'btn-flat': '!bg-transparent',
 
     // modals
     overlay:
@@ -52,19 +51,34 @@ export default <UnocssNuxtOptions>defineConfig({
     'modal-content': 'flex flex-col gap-15px',
     'modal-footer': 'flex justify-between',
     modal:
-      'mt-100px md:w-50% bg-white dark:bg-slate-900 p-20px md:rounded-md flex flex-col gap-20px',
+      'mt-100px md:w-500px bg-white dark:bg-slate-900 p-20px md:rounded-md flex flex-col gap-20px',
 
     // fields
     field: 'flex flex-col gap-10px',
     'field-label':
-      'block text-gray-700 dark:text-slate-50 font-semibold text-base',
+      'block text-gray-700 dark:text-slate-50 font-medium text-base',
     'field-invalid': 'border-red-500',
     'field-error': 'block text-xs text-red-500',
 
     'field-string':
-      'border-styling text-sm px-10px py-8px box-border text-gray-700 bg-white dark:text-slate-50 dark:bg-slate-900 focus:border-indigo-500',
+      'transition-all duration-300 border-styling text-sm px-10px py-8px box-border text-gray-700 bg-white dark:text-slate-50 dark:bg-slate-900 focus:border-indigo-500',
 
     // icons
-    icon: 'fill-gray-700 dark:fill-slate-50 w-[inherit] h-[inherit]',
+    icon: 'w-[inherit] h-[inherit] fill-[inherit]',
+    'icon-secondary': 'icon !fill-gray-700 dark:!fill-slate-50',
+
+    // notifications
+    'notification-container':
+      'w-full max-h-144px md:max-h-298px md:pr-20px pb-10px flex flex-col gap-10px md:w-fit fixed top-0 md:top-20px md:right-0 z-20 overflow-y-auto',
+    notification:
+      'w-fit box-border border-styling border-coloring flex justify-between gap-10px items-center p-15px shadow-md',
+    'notification-icon-success':
+      'w-25px h-25px bg-emerald-200 fill-emerald-600 p-4px rounded-full',
+    'notification-icon-warn':
+      'w-25px h-25px bg-amber-200 fill-amber-600 p-4px rounded-full',
+    'notification-icon-danger':
+      'w-25px h-25px bg-rose-200 fill-rose-600 p-4px rounded-full',
+    'notification-message': 'text-sm text-styling inline-block w-200px',
+    'notification-dismiss': '',
   },
 })
