1cce81870db271e643fc0ec2e0b2ad2b3f2abeed
encrypted_fox <yaroslav.souchkov@gmail.com>
2024-09-02 11:35:20 +0300

    Make animations more smoothe

--- uno.config.ts
+++ uno.config.ts
@@ -21,7 +21,7 @@ export default <UnocssNuxtOptions>defineConfig({
     'border-styling': 'main-border-styling border-coloring',
 
     // buttons
-    btn: 'main-border-styling hover:opacity-85 focus:border-indigo-500 active:opacity-100 disabled:cursor-not-allowed cursor-pointer transition-all duration-300',
+    btn: 'main-border-styling hover:opacity-85 focus:border-indigo-500 active:opacity-100 disabled:cursor-not-allowed cursor-pointer transition-all duration-200',
     'btn-xs': 'text-xs px-8px py-4px',
     'btn-sm': 'text-xs px-8px py-4px',
     'btn-md': 'text-sm px-10px py-6px',
@@ -31,7 +31,7 @@ export default <UnocssNuxtOptions>defineConfig({
     'btn-round-md': 'p-6px !rounded-full w-20px h-20px',
     'btn-round-lg': 'p-8px !rounded-full w-25px h-25px',
     'btn-round-xl': 'p-10px !rounded-full w-25px h-25px',
-    link: 'w-fit transition-all duration-300 text-indigo-500 text-sm hover:text-indigo-400 active:text-indigo-500 cursor-pointer',
+    link: 'w-fit transition-all duration-200 text-indigo-500 text-sm hover:text-indigo-400 active:text-indigo-500 cursor-pointer',
     'btn-primary-disabled':
       'disabled:bg-indigo-300 dark:disabled:bg-indigo-950 dark:disabled:text-indigo-500',
     'btn-primary':
@@ -45,13 +45,13 @@ export default <UnocssNuxtOptions>defineConfig({
 
     // modals
     overlay:
-      'flex md:justify-center md:items-center fixed w-full h-screen z-10 top-0 bg-black/10 dark:bg-black/50',
+      'flex mt-50px md:mt-0 md:justify-center md:items-center fixed w-full h-screen z-10 top-0 bg-black/10 dark:bg-black/50',
     'modal-header':
       'pb-20px flex justify-between items-center division-styling border-0 border-b-2px',
     'modal-content': 'flex flex-col gap-15px',
     'modal-footer': 'flex justify-between',
     modal:
-      'mt-100px md:w-500px bg-white dark:bg-slate-900 p-20px md:rounded-md flex flex-col gap-20px',
+      'w-full md:w-500px bg-white dark:bg-slate-900 p-20px md:rounded-md flex flex-col gap-20px',
 
     // fields
     field: 'flex flex-col gap-10px',
@@ -61,7 +61,7 @@ export default <UnocssNuxtOptions>defineConfig({
     'field-error': 'block text-xs text-red-500',
 
     'field-string':
-      'transition-all duration-300 border-styling text-sm px-10px py-8px box-border text-gray-700 bg-white dark:text-slate-50 dark:bg-slate-900 focus:border-indigo-500',
+      'transition-all duration-200 border-styling text-sm px-10px py-8px box-border text-gray-700 bg-white dark:text-slate-50 dark:bg-slate-900 focus:border-indigo-500',
 
     // icons
     icon: 'w-[inherit] h-[inherit] fill-[inherit]',
@@ -69,9 +69,9 @@ export default <UnocssNuxtOptions>defineConfig({
 
     // notifications
     'notification-container':
-      'w-full max-h-144px md:max-h-298px md:pr-20px pb-10px flex flex-col gap-10px md:w-fit fixed top-0 md:top-20px md:right-0 z-20 overflow-y-auto',
+      'w-full max-h-144px md:max-h-298px md:pr-20px md:pb-10px flex flex-col gap-10px md:w-fit fixed bottom-0 md:bottom-unset md:top-0 md:top-20px md:right-0 z-20 overflow-y-auto',
     notification:
-      'w-fit box-border border-styling border-coloring flex justify-between gap-10px items-center p-15px shadow-md',
+      'w-full md:w-fit box-border border-styling border-coloring flex justify-between gap-10px items-center p-15px shadow-md',
     'notification-icon-success':
       'w-25px h-25px bg-emerald-200 fill-emerald-600 p-4px rounded-full',
     'notification-icon-warn':
@@ -79,6 +79,5 @@ export default <UnocssNuxtOptions>defineConfig({
     'notification-icon-danger':
       'w-25px h-25px bg-rose-200 fill-rose-600 p-4px rounded-full',
     'notification-message': 'text-sm text-styling inline-block w-200px',
-    'notification-dismiss': '',
   },
 })
