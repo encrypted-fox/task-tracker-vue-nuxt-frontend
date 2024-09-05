fa3564963d63cebfa222d702c0fe91f7a2e03e4b
encrypted_fox <yaroslav.souchkov@gmail.com>
2024-09-06 00:44:18 +0300

    Refactor middlewares

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,10 +1,11 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const localePath = useLocalePath()
   const userStore = useUserStore()
-  
+
   if (userStore.user.token) {
-    if (to.path.includes('auth')) return navigateTo({path: localePath('tasks')})
+    if (to.path.includes('auth'))
+      return navigateTo({ path: localePath('tasks') })
   } else if (!userStore.user.token && !to.path.includes('auth')) {
-    return navigateTo({path: localePath('auth'), query: {type: 'login'}})
+    return navigateTo({ path: localePath('auth'), query: { type: 'login' } })
   }
 })
