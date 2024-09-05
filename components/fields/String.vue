6254b8e8a5be5f08ebd0016b4d88b6e21458e0ed
encrypted_fox <yaroslav.souchkov@gmail.com>
2024-09-06 00:43:21 +0300

    Add prepend and append icons

--- components/fields/String.vue
+++ components/fields/String.vue
@@ -1,23 +1,40 @@
 <template lang="pug">
 .field
   label.field-label(:for='name', v-if='label') {{ label }}
-  
-  input.field-string.w-full(
-    :class='{ "!field-invalid": invalid }',
-    :id='name',
-    :type='type',
-    :placeholder='placeholder',
-    :value='value',
-    @input='input'
-  )
-  slot
+
+  .field-container
+    .field-icon-prepend(
+      v-html='iconPrepend',
+      @click='iconPrependAction',
+      v-if='iconPrepend'
+    )
+    input.field-string.w-full(
+      :class='{ "!field-invalid": invalid, "pl-40px": iconPrepend, "pr-40px": iconAppend }',
+      :id='name',
+      :type='type',
+      :placeholder='placeholder',
+      :value='value',
+      @input='input'
+    )
+    .field-icon-append(
+      v-html='iconAppend',
+      @click='iconPrependAction',
+      v-if='iconAppend'
+    )
+    slot
 </template>
 
 <script lang="ts" setup>
+import IconClose from '~/assets/icons/close.svg?raw'
+
 defineProps<{
   name: string
   value?: string
   placeholder?: string
+  iconPrepend?: string
+  iconAppend?: string
+  iconPrependAction?: string
+  iconAppendAction?: string
   type?: string
   label?: string
   invalid?: boolean
@@ -25,7 +42,7 @@ defineProps<{
 
 const emit = defineEmits<{ input: [value: string] }>()
 
-const input = (e: Event) => {
+const input = (e: Event): void => {
   const val = (e.target as HTMLInputElement).value
 
   emit('input', val)
