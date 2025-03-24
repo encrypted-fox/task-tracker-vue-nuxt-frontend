15888e335bb29e9973e56e4c581148279e0d7a05
encrypted_fox <yaroslav.souchkov@gmail.com>
2025-03-24 13:47:24 +0300

    Add projects page

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,5 +1,16 @@
 <template lang="pug">
 main.page
+  PageHeader(
+    :isItemsFolded='isItemsFolded',
+    :title='$t("pages.projects.title")',
+    :search='search',
+    :is-filters-button-shown='true',
+    :is-sort-button-shown='true',
+    :is-fold-button-shown='false',
+    @switch-is-items-folded='switchIsItemsFolded',
+    @changeSearch='changeSearch'
+  )
+  PageList(:path='"projects"', :search='""', :filters='{}')
 </template>
 
 <script setup lang="ts"></script>
