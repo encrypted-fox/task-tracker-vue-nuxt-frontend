124d67445fedc2beed20ff727ca39df6fad319a3
encrypted_fox <yaroslav.souchkov@gmail.com>
2025-03-24 13:30:46 +0300

    Add initial editor field configuration, needs further extending

---
+++ components/fields/Editor.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  TiptapEditorContent(:editor="editor" class="editor")
+</template>
+<script setup lang="ts">
+//TODO: needs further extenting
+
+const editor = useEditor({
+  content: "<span>I'm running Tiptap with Vue.js. 🎉</span>",
+  extensions: [TiptapStarterKit],
+});
+
+onBeforeUnmount(() => {
+  unref(editor).destroy();
+});
+
+const emit = defineEmits(['update']);
+</script>
+
+<style lang="scss" scoped>
+@use '~/assets/scss/utils/colors.scss' as *;
+
+.editor {
+ padding: 10px;
+ border: 2px solid $gray-300;
+
+ :deep(p) { 
+  margin: 0 !important;
+}
+}
+</style>
